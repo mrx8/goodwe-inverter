78fb07709921bdeb15bcdf56c358980098ab26dd
Andreas Vierengel <avierengel@vwd.com>
2024-09-08 20:46:21 +0200

    once more bugfix

--- src/_bricks/sensors/read-timestamp.mjs
+++ src/_bricks/sensors/read-timestamp.mjs
@@ -9,13 +9,13 @@ export default Factory
     readTimestamp (register) {
       const index = this.getIndexFromRegister(register)
 
-      const year = 2000 + this.message[index]
-      const month = this.message[index + 1]
-      const day = this.message[index + 2]
-      const hour = this.message[index + 3]
-      const minute = this.message[index + 4]
-      const second = this.message[index + 5]
+      const year = (2000 + this.message[index]).toString().padStart(4, '0')
+      const month = this.message[index + 1].toString().padStart(2, '0')
+      const day = this.message[index + 2].toString().padStart(2, '0')
+      const hour = this.message[index + 3].toString().padStart(2, '0')
+      const minute = this.message[index + 4].toString().padStart(2, '0')
+      const second = this.message[index + 5].toString().padStart(2, '0')
 
-      return new Date(`${year}-${month}-${day} ${hour}:${minute}:${second}`).toISOString()
+      return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}`).toISOString()
     },
   })

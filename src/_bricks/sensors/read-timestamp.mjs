3a14f80772b96e00db92a95d69a70c260bc5e995
Andreas Vierengel <avierengel@vwd.com>
2024-09-08 20:27:49 +0200

    fix reading of timestamp

--- src/_bricks/sensors/read-timestamp.mjs
+++ src/_bricks/sensors/read-timestamp.mjs
@@ -10,12 +10,12 @@ export default Factory
       const index = this.getIndexFromRegister(register)
 
       const year = 2000 + this.message[index]
-      const month = --this.message[index + 1]
+      const month = this.message[index + 1]
       const day = this.message[index + 2]
       const hour = this.message[index + 3]
       const minute = this.message[index + 4]
       const second = this.message[index + 5]
 
-      return new Date(year, month, day, hour, minute, second).toISOString()
+      return new Date(`${year}-${month}-${day}T${hour}:${minute}:${second}CET`).toISOString()
     },
   })
